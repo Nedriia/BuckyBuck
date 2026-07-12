df87b1bc41accb35a07c0c151e400e47590266f2
Nedriia <arthur.favre@hotmail.com>
2026-07-12 19:30:44 +0200

    Add VLD in main for now

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include "Disassembler.h"
 
+#ifdef LEAK_DETECTOR
+	#include <vld.h>
+	#define ENABLE_GLOBAL_LEAK_DETECTION() VLDGlobalEnable()
+	#define DISABLE_GLOBAL_LEAK_DETECTION() VLDGlobalDisable()
+
+	#define ENABLE_SPECIFIC_LEAK_DETECTION() VLDEnable()
+	#define DISABLE_SPECIFIC_LEAK_DETECTION() VLDDisable()
+#else
+	#define ENABLE_GLOBAL_LEAK_DETECTION() ((void)0)
+	#define DISABLE_GLOBAL_LEAK_DETECTION() ((void)0)
+
+	#define ENABLE_SPECIFIC_LEAK_DETECTION() ((void)0)
+	#define DISABLE_SPECIFIC_LEAK_DETECTION() ((void)0)
+#endif
+
 int main()
 {
 	Disassembler oDisassembler;
-
 	return 0;
 }
